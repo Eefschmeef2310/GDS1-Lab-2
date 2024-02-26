df4c8adb1544cce9b71a212de728891e15294f01
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-02-26 17:21:40 +1100

    Update GameManager.gd

--- GameManager.gd
+++ GameManager.gd
@@ -1,42 +1,31 @@
 extends Node
 
-var coin_counter = 0
-var score = 0
-var lives = 3
+#Because we know these variables will only be ints, we can cast them as such
+var coin_counter : int = 0
+var score : int = 0
+var lives : int = 3
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
+#You don't need to have pass in a method if ther are other lines of code
 func _on_timer_timeout():
 	#Kill the player
-	pass # Replace with function body.
+	pass
 	
 func collect_coin():
 	coin_counter += 1
 	if(coin_counter >= 100):
-		#gain 1-up
+		gain_1up()
 		#play 1-up sound
 		coin_counter = 0
-	pass
 	
 func increase_score(amount):
 	score += amount
-	pass
+	#logic here for stacking score? we'll see how xander sorts out the character controller
 	
 func gain_1up():
 	lives += 1
-	pass
 	
 func reduce_lives():
 	lives -= 0
 	if(lives == 0):
 		#game over
 		pass
-	pass
