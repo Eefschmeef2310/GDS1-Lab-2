cb44e69e5b57298ab64b73c0d52b81eafc3dc46f
Pihoqub <111715046+Pihoqub@users.noreply.github.com>
2024-02-26 16:41:01 +1100

    Update GameManager.gd

--- GameManager.gd
+++ GameManager.gd
@@ -2,6 +2,7 @@ extends Node
 
 var coin_counter = 0
 var score = 0
+var lives = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,6 +26,17 @@ func collect_coin():
 		coin_counter = 0
 	pass
 	
-func score_increase(amount):
+func increase_score(amount):
 	score += amount
+	pass
+	
+func gain_1up():
+	lives += 1
+	pass
+	
+func reduce_lives():
+	lives -= 0
+	if(lives == 0):
+		#game over
+		pass
 	pass
