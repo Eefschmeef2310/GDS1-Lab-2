0389c4f7f3852a2f4acc46f7cc8a25cdc2f4db5d
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-02-27 20:05:24 +1100

    fixed the camera jitter

--- levels/level_camera.gd
+++ levels/level_camera.gd
@@ -2,5 +2,5 @@ extends Camera2D
 
 @onready var player = $"../CharacterBody2D"
 
-func _process(_delta):
+func _physics_process(_delta):
 	global_position.x = max(global_position.x, player.global_position.x)
