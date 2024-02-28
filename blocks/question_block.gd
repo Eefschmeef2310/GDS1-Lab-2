c6ab8206eed4f58e7dbc85e30d08d9ed9cd8fd7a
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-02-28 18:17:56 +1100

    finished coin popup

--- blocks/question_block.gd
+++ blocks/question_block.gd
@@ -2,7 +2,7 @@ extends Block
 
 @onready var animation_player = $AnimationPlayer
 
-const COIN : PackedScene = preload("res://ui/score_popup.tscn")
+const COIN : PackedScene = preload("res://blocks/coin_popup.tscn")
 
 var hit : bool = false
 
@@ -10,7 +10,7 @@ func _on_player_hit_box_body_entered(body):
 	if body.is_in_group("player") and !hit:
 		
 		var coin = COIN.instantiate()
-		coin.global_position = global_position
+		coin.global_position = $CoinMarker.global_position
 		get_tree().root.add_child(coin)
 		
 		animation_player.play("hit")
