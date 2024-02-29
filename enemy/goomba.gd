cbff65b29294a2f81aacea3a30b41a4039e838f1
Clevertop <41929769+Clevertop@users.noreply.github.com>
2024-02-29 15:42:22 +1100

    improved goomba

--- enemy/goomba.gd
+++ enemy/goomba.gd
@@ -10,11 +10,12 @@ extends Ground_Enemy
 
 
 func _on_area_2d_body_entered(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("player") && !dying:
 		dying = true
 		animated_sprite_2d.set_animation("dead")
 		collision_shape_2d.set_deferred("disabled",true)
-		body.velocity.y += -300
+		
+		body.velocity.y = -300
 
 
 func _on_direction_hitbox_body_entered(body):
