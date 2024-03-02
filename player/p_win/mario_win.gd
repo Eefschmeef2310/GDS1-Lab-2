cd10be24d715e284a910a91740d1ddca7d66fd6d
Clevertop <41929769+Clevertop@users.noreply.github.com>
2024-03-02 14:13:15 +1100

    win screen implementation

--- player/p_win/mario_win.gd
+++ player/p_win/mario_win.gd
@@ -67,3 +67,7 @@ func _physics_process(delta):
 		#velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	#move_and_slide()
+
+
+func _on_win_music_finished():
+	get_tree().change_scene_to_file("res://levels/you_win.tscn")
